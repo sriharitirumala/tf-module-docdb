72acda32cdaeb8ca48da12e57bf54000864fb125
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 11:37:35 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
-#resource "aws_docdb_cluster" "main" {
-#  cluster_identifier      = "${var.env}-docdb"
-#  engine                  =  var.engine
-#  engine_version          = "4.0.0"
-#  master_username         = data.aws_ssm_parameter.user.value
-#  master_password         = data.aws_ssm_parameter.pass.value
-#  backup_retention_period = var.backup_retention_period
-#  preferred_backup_window = var.preferred_backup_window
-#  skip_final_snapshot     = true
-#}
-#
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${var.env}-docdb"
+  engine                  =  var.engine
+  engine_version          = "4.0.0"
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = true
+  db_subnet_group_name    =  aws_docdb_subnet_group.main.name
+  kms_key_id              = data.aws_kms_key.key.arn
+  storage_encrypted       = true
+}
+
 
 resource "aws_docdb_subnet_group" "default" {
   name       = "${var.env}-docdb"
