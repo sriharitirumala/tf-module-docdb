cf552d6a76a45be107a34c8be20de7e24e43663c
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 11:52:28 +0530

    adding vpc subnets, routes, route table associations

--- vars.tf
+++ vars.tf
@@ -6,4 +6,9 @@ variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
-variable "storage_encrypted" {}
+variable "storage_encrypted" {
+  default = true
+}
+
+variable "no_of_instances" {}
+variable "instance_class" {}
