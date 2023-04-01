a16d5619631584d99f7d2fb0defdb22d4cab8b93
sriharitirumala <sritirumala30@gmail.com>
2023-04-01 16:55:06 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_docdb_cluster" "main" {
   skip_final_snapshot     = true
   kms_key_id              = data.aws_kms_key.key.arn
   storage_encrypted       = var.storage_encrypted
+  vpc_security_group_ids = [aws_security_group.main.id]
 
 }
 
@@ -18,6 +19,36 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   cluster_identifier    = aws_docdb_cluster.main.id
   instance_class        = var.instance_class
 }
+
+
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id = var.vpc_id
+
+
+  ingress {
+    description      = "DOCDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = merge (
+    var.tags,
+    { Name = "docdb-${var.env}" }
+  )
+}
+
+
 
 
 resource "aws_docdb_subnet_group" "main" {
--- vars.tf
+++ vars.tf
@@ -12,3 +12,6 @@ variable "storage_encrypted" {
 
 variable "no_of_instances" {}
 variable "instance_class" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
+
