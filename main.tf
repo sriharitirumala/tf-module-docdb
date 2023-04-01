c465e8dea72ba78dd36b08e83c87bceca5a0e9df
sriharitirumala <sritirumala30@gmail.com>
2023-04-01 14:05:14 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -29,3 +29,35 @@ resource "aws_docdb_subnet_group" "main" {
     { Name = "${var.env}-subnet-group" }
   )
 }
+
+
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.docdb.catalogue"
+  type  = "string"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@dev-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.docdb.catalogue"
+  type  = "string"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@dev-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+
+resource "aws_ssm_parameter" "docdb_endpoint" {
+  name  = "${var.env}.docdb.endpoint"
+  type  = "string"
+  value = aws_docdb_cluster.main.endpoint
+}
+
+resource "aws_ssm_parameter" "docdb_user" {
+  name  = "${var.env}.docdb.user"
+  type  = "string"
+  value = data.aws_ssm_parameter.user.value
+}
+
+resource "aws_ssm_parameter" "docdb_pass" {
+  name  = "${var.env}.docdb.pass"
+  type  = "string"
+  value = data.aws_ssm_parameter.pass.value
+}
